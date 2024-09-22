0a38a332ec94d7c17d3d1b3a255c1a1b32c67ad0
Rahul-0410 <rgs786999@gmail.com>
2024-09-22 22:25:51 +0530

    added login and register

--- client/src/Pages/LoginPage.jsx
+++ client/src/Pages/LoginPage.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
 
 function LoginPage() {
+  const [email,setEmail]= useState('');
+  const [password,setPassword]= useState
   return (
     <div className='mt-4 grow flex items-center justify-around'>
       <div className='-mt-64'>
       <h1 className='text-4xl text-center mb-4'>Login</h1>
       <form className='max-w-md mx-auto '>
-        <input type='email' placeholder='your@email.com'/>
-        <input type="password" placeholder='password' />
+        <input type='email' placeholder='your@email.com' value={email}
+        onChange={(e)=>{setEmail(e.target.value)}}/>
+        <input type="password" placeholder='password' value={password}
+        onChange={(e)=>{
+          setPassword(e.target.value)
+        }} />
         <button className='primary'>Login</button>
         <div className='text-center py-2 mt-1 text-gray-500'>
           Don't have an account? <Link className='underline text-black' to={'/register'}>Register now</Link>
--- client/src/Pages/RegisterPage.jsx
+++ client/src/Pages/RegisterPage.jsx
@@ -1,24 +1,52 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function RegisterPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  function registerUser(e){
+    e.preventDefault();
+    
+  }
   return (
-    <div className='mt-4 grow flex items-center justify-around'>
-      <div className='-mt-64'>
-      <h1 className='text-4xl text-center mb-4'>Register</h1>
-      <form className='max-w-md mx-auto '>
-        <input type='text' placeholder='username'/>
-        <input type='email' placeholder='your@email.com'/>
-        <input type="password" placeholder='password' />
-        <button className='primary'>Login</button>
-        <div className='text-center py-2 mt-1 text-gray-500'>
-        Already have an account? <Link className='underline text-black' to={'/login'}>Login now</Link>
-        </div>
-      </form>
+    <div className="mt-4 grow flex items-center justify-around">
+      <div className="-mt-64">
+        <h1 className="text-4xl text-center mb-4">Register</h1>
+        <form className="max-w-md mx-auto" onSubmit={registerUser}>
+          <input
+            type="text"
+            placeholder="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="your@email.com"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <button className="primary">Register</button>
+          <div className="text-center py-2 mt-1 text-gray-500">
+            Already have an account?{" "}
+            <Link className="underline text-black" to={"/login"}>
+              Login now
+            </Link>
+          </div>
+        </form>
       </div>
-     
     </div>
-  )
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
